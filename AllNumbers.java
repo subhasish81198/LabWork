dbb9dc7941756479fe2b9278b1137ca1beec2616
subhasish81198 <93348704+subhasish81198@users.noreply.github.com>
2023-09-25 15:22:30 +0530

    Add files via upload
    Create a Java program that uses a while loop to find the sum of all numbers from 1 to 100.
    
    Create a Java program that prints all even numbers from 1 to 20 using a while loop

---
+++ AllNumbers.java
@@ -0,0 +1,15 @@
+package Javanew;
+public class AllNumbers {
+	public static void main (String[] args) { 
+		int i = 0;       // i is 0 Here
+
+		    while (i<=20) {
+		    	if(i%2==0) {
+		    		System.out.println("even number is:"+ i);
+		    	}
+		      
+		      i++;
+
+		    }
+}
+}
---
+++ Sum.java
@@ -0,0 +1,19 @@
+package Javanew;
+public class Sum {
+
+	public static void main(String[] args) {
+		int sum = 0; 
+		int i = 0;       // i is 0 Here
+
+		    while (i<=100) {
+
+		      sum += i; 
+		      i++;
+
+		    }
+
+		  System.out.println("The sum is " + sum);
+}
+}
+	
+
